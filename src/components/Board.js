1bab115c6284afa872b6bed00c2d7f1d022230a7
Hong <hkhong91@gmail.com>
2020-09-09 22:42:06 +0900

    board basic style

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import './Board.css'
 
 function Board({ id, title, createdBy, createdName }) {
-  return <h3>{title}</h3>
+  return <div className="boards">
+    <h3>{title}</h3>
+    <h5>작성자: {createdName}</h5>
+  </div>
 }
 
 Board.prototype = {
