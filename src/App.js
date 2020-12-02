6b8695823c24ac80a369660f55c5d2bbe472bba1
Rupee <rupee@within.co.kr>
2020-12-02 19:48:46 +0900

    react state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
+/* eslint-disable */
+
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let [글제목, 글제목변경] = useState([
+    '남자 코트 추천',
+    '강남 우동맛집',
+    '파이썬독학'
+  ]);
+  let [따봉, 따봉변경] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+      <button onClick={() => {
+        var 변경할글제목 = [...글제목];
+        변경할글제목[0] = '여자 코트 추천';
+        글제목변경(변경할글제목);
+      }}>버튼</button>
+      <div className="list">
+        <h4>{글제목[0]} <span onClick={() => { 따봉변경(따봉 + 1) }}>👍</span> {따봉} </h4>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h4>{글제목[1]}</h4>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h4>{글제목[2]}</h4>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
     </div>
   );
 }
