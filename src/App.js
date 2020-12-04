32ec057e9b779e0f3c60bc0e6f807ee939e26428
Rupee <rupee@within.co.kr>
2020-12-04 17:21:50 +0900

    rename variable

--- src/App.js
+++ src/App.js
@@ -2,36 +2,33 @@ import React, { useState } from 'react'
 import './App.css'
 
 function App() {
-  let [clickedBoardIndex, setClickedBoardIndex] = useState(0)
-  let [modal, setModal] = useState(false)
   let [boards, setBoards] = useState([{
     title: '남자 코트 추천',
     content: '내용1',
     createdAt: '2월 17일 발행',
-    likeCount: 0,
-    liked: false
+    likeCount: 0
   }, {
     title: '강남 우동맛집',
     content: '내용2',
     createdAt: '2월 18일 발행',
-    likeCount: 0,
-    liked: false
+    likeCount: 0
   }, {
     title: '파이썬독학',
     content: '내용3',
     createdAt: '2월 19일 발행',
-    likeCount: 0,
-    liked: false
+    likeCount: 0
   }])
+  let [modal, setModal] = useState(false)
+  let [clickedBoard, setClickedBoard] = useState({})
 
   function clickBoard(index) {
-    setClickedBoardIndex(index)
+    setClickedBoard(boards[index])
     if (!modal)
       setModal(true)
   }
 
   function likeBoard(index) {
-    var newBoards = [...boards]
+    var newBoards = [...boards] // deep copy
     newBoards[index].likeCount++
     setBoards(newBoards)
   }
@@ -53,7 +50,7 @@ function App() {
       
       {
         modal === true
-          ? <Modal boards={boards} clickedBoardIndex={clickedBoardIndex} /> // child component
+          ? <Modal board={clickedBoard} /> // child component
           : null
       }
     </div>
@@ -61,11 +58,13 @@ function App() {
 }
 
 function Modal(props) {
+  let board = props.board
   return (
     <div className="modal">
-      <h2>{props.boards[props.clickedBoardIndex].title}</h2>
-      <p>{props.boards[props.clickedBoardIndex].createdAt}</p>
-      <p>{props.boards[props.clickedBoardIndex].content}</p>
+      <h2>{board.title}</h2>
+      <p>{board.createdAt}</p>
+      <p>{board.content}</p>
+      <p>{board.likeCount}</p>
     </div>
   )
 }
